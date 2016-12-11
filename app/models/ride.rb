357073dd983c7e8c590b7bcbb7fde871d4d3bdc8
Joe Lee <slee264@binghamton.edu>
2016-12-11 06:10:32 +0000

    minor changes to gemfile

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -69,7 +69,6 @@ class Ride < ActiveRecord::Base
         d = Date.new(ride['date(1i)'].to_i, ride['date(2i)'].to_i, ride['date(3i)'].to_i)
         
         rides = Ride.where('DATE(dateAndTime) = ?', d)
-        # debugger
         return rides.where(destination_location: ride[:destination], departure_location: ride[:departure])
     end
 end
