54ca1d89c148fd1b92633f9e736cafaa8e203779
Joe Lee <slee264@binghamton.edu>
2016-12-14 01:54:35 +0000

    search.haml not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,8 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out"
+    @message = "You have been logged out."
+    redirect_to root_path
   end
   
 end
