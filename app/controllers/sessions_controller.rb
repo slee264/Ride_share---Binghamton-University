165b32c0cbabbb8996e6142ef723b08f86c17540
Joe Lee <slee264@binghamton.edu>
2016-12-14 02:20:27 +0000

    everything done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,9 +19,7 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "You have been logged out."
-    redirect_to logout_path
-    # @message = "You have been logged out."
+    @message = "You have been logged out."
   end
   
 end
