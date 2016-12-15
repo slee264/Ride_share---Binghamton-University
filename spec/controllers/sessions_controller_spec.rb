5503f80235d7d27737b454050ea6644c91a8e7e6
Joe Lee <slee264@binghamton.edu>
2016-12-15 19:27:33 +0000

    tests done

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -19,10 +19,7 @@ RSpec.describe SessionsController, type: :controller do
             post :create, {:user => {:first_name => 'Matt', :last_name => 'Price', :email => 'mprice3@binghamton.edu', :password => 'goodmusic'}, :post => {:password_confirmation => 'goodmusic'}}
             @controller = SessionsController.new
             post :create, {:session => {:email => 'mprice3@binghamton.edu', :password => 'goodmusic'}}
-            #assert_template 'users/show'
-           #assert_select "h2", "Invalid email/password combination"
-            # assert_select "h2", "Welcome mprice3!"
-            assert_tag :tag => 'h2'
+            response.should redirect_to(user_path(1))
         end
         
          it 'fails to login a user with an invalid email/password combo' do
